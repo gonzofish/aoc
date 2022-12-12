3a59c513afa942a30abd6901b100fbdd86f26c16
Matt Fehskens <matt.fehskens@gmail.com>
2022-12-12 11:12:00 -0500

    Day 8 part 1 cleanup/speedup
    Time to execute went from 19-22ms to 15-16ms for full input

--- 2022/08/a.ts
+++ 2022/08/a.ts
@@ -11,91 +11,111 @@ function run() {
     input[lastRowIndex].length + // whole last row
     lastInnerRowIndex * 2; // first and last of all other rows
 
-  // check inner trees, starting at row[1] and ending at row[lastRowIndex - 1]
+  // check inner trees, ignore the first and last row
   for (let rowIndex = 1; rowIndex < lastRowIndex; rowIndex = rowIndex + 1) {
     const row = input[rowIndex];
 
+    // only check inner trees, ignore the first & last tree
     for (let colIndex = 1; colIndex < row.length - 1; colIndex = colIndex + 1) {
-      const tree = input[rowIndex][colIndex];
-      let isVisible = true;
-      let checkRowIndex = 1;
-
-      // VISIBLE FROM TOP?
-      while (isVisible && checkRowIndex <= rowIndex) {
-        const previousTree = input[checkRowIndex - 1][colIndex];
-
-        if (previousTree >= tree) {
-          isVisible = false;
-        }
-
-        checkRowIndex = checkRowIndex + 1;
-      }
-
-      if (isVisible) {
+      if (
+        checkIsTopVisible(input, rowIndex, colIndex) ||
+        checkIsBottomVisible(input, rowIndex, colIndex, lastInnerRowIndex) ||
+        checkIsLeftVisible(input[rowIndex], colIndex) ||
+        checkIsRightVisible(input[rowIndex], colIndex, row.length - 2)
+      ) {
         visibleTrees = visibleTrees + 1;
-        continue;
-      }
-
-      checkRowIndex = lastInnerRowIndex;
-      isVisible = true;
-
-      // VISIBLE FROM BOTTOM?
-      while (isVisible && checkRowIndex >= rowIndex) {
-        const previousTree = input[checkRowIndex + 1][colIndex];
-
-        if (previousTree >= tree) {
-          isVisible = false;
-        }
-
-        checkRowIndex = checkRowIndex - 1;
-      }
-
-      if (isVisible) {
-        visibleTrees = visibleTrees + 1;
-        continue;
-      }
-
-      let checkColIndex = 1;
-      isVisible = true;
-
-      // VISIBLE FROM LEFT?
-      while (isVisible && checkColIndex <= colIndex) {
-        const previousTree = input[rowIndex][checkColIndex - 1];
-
-        if (previousTree >= tree) {
-          isVisible = false;
-        }
-
-        checkColIndex = checkColIndex + 1;
-      }
-
-      if (isVisible) {
-        visibleTrees = visibleTrees + 1;
-        continue;
-      }
-
-      checkColIndex = row.length - 2;
-      isVisible = true;
-
-      // VISIBLE FROM RIGHT?
-      while (isVisible && checkColIndex >= colIndex) {
-        const previousTree = input[rowIndex][checkColIndex + 1];
-
-        if (previousTree >= tree) {
-          isVisible = false;
-        }
-
-        checkColIndex = checkColIndex - 1;
-      }
-
-      if (isVisible) {
-        visibleTrees = visibleTrees + 1;
-        continue;
       }
     }
   }
 
   console.info(visibleTrees);
+}
+
+function checkIsTopVisible(
+  input: number[][],
+  treeRow: number,
+  treeCol: number
+) {
+  const tree = input[treeRow][treeCol];
+  let rowIndex = 1;
+
+  while (rowIndex <= treeRow) {
+    // comparison tree is in the same column but in a row above
+    // the current tree
+    const treeAbove = input[rowIndex - 1][treeCol];
+
+    if (treeAbove >= tree) {
+      return false;
+    }
+
+    rowIndex = rowIndex + 1;
+  }
+
+  return true;
+}
+
+function checkIsBottomVisible(
+  input: number[][],
+  treeRow: number,
+  treeCol: number,
+  startAt: number
+) {
+  const tree = input[treeRow][treeCol];
+  let rowIndex = startAt;
+
+  while (rowIndex >= treeRow) {
+    // comparison tree is in the same column but in a row below
+    // the current tree
+    const treeBelow = input[rowIndex + 1][treeCol];
+
+    if (treeBelow >= tree) {
+      return false;
+    }
+
+    rowIndex = rowIndex - 1;
+  }
+
+  return true;
+}
+
+function checkIsLeftVisible(input: number[], treeCol: number) {
+  const tree = input[treeCol];
+  let colIndex = 1;
+
+  while (colIndex <= treeCol) {
+    // comparison tree is in the same row but in a column to the left
+    // of the current tree
+    const treeToLeft = input[colIndex - 1];
+
+    if (treeToLeft >= tree) {
+      return false;
+    }
+
+    colIndex = colIndex + 1;
+  }
+
+  return true;
+}
+
+function checkIsRightVisible(
+  input: number[],
+  treeCol: number,
+  startAt: number
+) {
+  const tree = input[treeCol];
+  let colIndex = startAt;
+
+  while (colIndex >= treeCol) {
+    const treeToRight = input[colIndex + 1];
+
+    if (treeToRight >= tree) {
+      return false;
+    }
+
+    colIndex = colIndex - 1;
+  }
+
+  return true;
 }
 
 measurePerf(run);
